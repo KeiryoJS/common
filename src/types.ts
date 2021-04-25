00d39a214fac6cd552d0af8dbfad4f16b276c851
melike2d <ML2DOfficial@gmail.com>
2021-04-25 04:49:33 -0700

    :fire: remove copyright header

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,17 @@
-/*
- * NeoCord
- * Copyright 2021 melike2d
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 export type Tuple<A = any, B = any> = [ A, B ];
 export type Dictionary<V = any, K extends PropertyKey = string> = Record<K, V>;
 
 export type Class<T = any> = {
   new(...args: any[]): T
+}
+
+/**
+ * Used for compatibility between different types of event emitters.
+ */
+export interface EventEmitterLike {
+  emit(event: string, ...args: any[]): boolean | void;
+
+  addListener(event: string, listener: (...args: any[]) => void): EventEmitterLike | any;
+
+  removeListener(event: string, listener: (...args: any[]) => void): EventEmitterLike | any;
 }
